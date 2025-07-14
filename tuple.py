6cc66a0089edc82319e42b1fcf1346b479edf98d
krisharavsj <krisharsv@gmail.com>
2025-07-14 20:27:27 +0530

    tuple.py

---
+++ tuple.py
@@ -0,0 +1,12 @@
+tuple=(2,3,4,5,6,7,4.56,'krish')
+print(tuple)
+run=(1,2,3,4,5)
+run=run+(9,)
+print (run)
+numb=(44,65,23,44,50,80,44,32,83,89,32,32)
+print(numb.count(44))
+print(numb.count(32))
+slice=numb[3:5]
+print(slice)
+slice=numb[:5]
+print (slice)
